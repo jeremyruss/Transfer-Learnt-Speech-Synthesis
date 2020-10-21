1d3ecbca95905a0f60b3e2a457f668b3c81ba217
jeremyruss <57459555+jeremyruss@users.noreply.github.com>
2020-10-21 23:01:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,29 @@ window.addEventListener("scroll", function() {
     var offset = window.pageYOffset;
     var pos = - offset * 0.1;
     document.body.style.backgroundPositionY = pos + "px";
+    console.log(window.screen.width);
 });
+
+window.onload = function () {
+    width = this.innerWidth;
+    if (width < 1400) {
+        document.body.style.background = 'none';
+        document.getElementById("container").style.width = width - 150 + "px";
+        document.getElementById("container").style.marginRight = "50px";
+    } else {
+        document.body.style.background = "url('files/Boris.png') no-repeat right top fixed";
+        document.getElementById("container").style.width = "1000px";
+    }
+}
+
+window.onresize = function () {
+    width = this.innerWidth;
+    if (width < 1400) {
+        document.body.style.background = 'none';
+        document.getElementById("container").style.width = width - 150 + "px";
+        document.getElementById("container").style.marginRight = "50px";
+    } else {
+        document.body.style.background = "url('files/Boris.png') no-repeat right top fixed";
+        document.getElementById("container").style.width = "1000px";
+    }
+}
